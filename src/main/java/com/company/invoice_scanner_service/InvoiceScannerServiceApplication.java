d98159ac9812554eb1ffcbab27fdb6904a0644dc
Jeetendra Salgaonkar <jeetendra-atul.salgaonkar@sixt.com>
2025-03-12 21:00:51 +0100

    Initial Commit

---
+++ src/main/java/com/company/invoice_scanner_service/InvoiceScannerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.invoice_scanner_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceScannerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceScannerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/invoice_scanner_service/InvoiceScannerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.invoice_scanner_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceScannerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
